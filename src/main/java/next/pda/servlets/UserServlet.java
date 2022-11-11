203c8e4debe04f0bd41318df0b3174f776b99ef8
said hasnaoui <79711559+Hasnaoui-Said@users.noreply.github.com>
2022-11-11 17:24:41 +0100

    Pda 33 servlet users (#14)
    * PDA-36 bugFix: update Type exercise type String
    
    * PDA-33 create servlet for user and add userDao

--- src/main/java/next/pda/servlets/UserServlet.java
+++ src/main/java/next/pda/servlets/UserServlet.java
@@ -6,13 +6,27 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import next.pda.dao.daoImp.UserDaoImp;
+import next.pda.entity.Responsable;
+import next.pda.entity.User;
+import next.pda.enu.Roles;
+import next.pda.enu.TypesResponsable;
+import next.pda.services.ParticipantService;
+import next.pda.services.ResponsableService;
+import next.pda.services.serviceImp.ParticipantServiceImp;
+import next.pda.services.serviceImp.ResponsableServiceImp;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "UserServlet", value = "/pda/v1/users")
 public class UserServlet extends HttpServlet {
+    ParticipantService participantService = new ParticipantServiceImp();
+    ResponsableService responsableService = new ResponsableServiceImp();
+    UserDaoImp userDao = new UserDaoImp();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<User> users = this.userDao.getAll();
         request.setAttribute("active", "users");
         getServletContext().getRequestDispatcher("/pda/home.jsp").forward(request, response);
     }
